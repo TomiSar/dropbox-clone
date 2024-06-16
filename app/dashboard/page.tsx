6b03f1b1a7146a777229643fa2a3a314a0da0c9b
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2024-06-16 21:23:15 +0300

    Homepage implementation updated for more precise. Implemented image Drag and Drop functionality with Dropzone in application.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import Dropzone from '@/components/Dropzone';
+import { auth } from '@clerk/nextjs';
 import React from 'react';
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const { userId } = auth();
+  return (
+    <div>
+      {/* <h1>Dashboard</h1> */}
+      <Dropzone />
+    </div>
+  );
 }
 
 export default Dashboard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,51 @@
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className=''>
-      <h1 className='text-red-500 dark:text-blue-500'>Dropbox Clone</h1>
+      <div className='flex flex-col lg:flex-row items-center bg-[#2B2929] dark:bg-slate-800'>
+        <div className='p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5'>
+          <h1 className='text-5xl font-bold'>
+            Welcome to Dropbox
+            <br />
+            <br />
+            Storing everything for you and your business needs. All in one
+            place.
+          </h1>
+          <p className='pb-20'>
+            Enhance your personal storage with Dropbox, offering a simple and
+            efficient way to upload, organize, and access files from anywhere.
+            Securely store important documents and media, and expiernce the
+            convenience of easy file management and sharing in one centralized
+            solution.
+          </p>
+          <Link
+            className='flex group cursor-pointer bg-blue-500 p-5 w-fit'
+            href='/dashboard'
+          >
+            Try it for free!
+            <ArrowRight className='ml-10 group-hover:animate-pulse' />
+          </Link>
+        </div>
+
+        <div className='bg-[#1E1919] dark:bg-slate-800 h-full p-10'>
+          <video autoPlay loop muted className='rounded-lg'>
+            <source
+              src='https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4'
+              type='video/mp4'
+            />
+            Your browser does not support thie video tag
+          </video>
+        </div>
+      </div>
+      <p className='text-center font-bold text-xl pt-5'>Disclaimer</p>
+      <p className='text-center font-light p-2'>
+        This video is made for informational and educational purposes only. We
+        do nto affiliate with Dropbox or any of its subsidiaries in any form.
+        Copyright disclaimer under section 107 of the Copyright Act 1976,
+        allowance is made for "fair use" of this video for education purposes.
+      </p>
     </main>
   );
 }
