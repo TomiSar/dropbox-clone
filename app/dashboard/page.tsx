9733425e8046f7d0b168ab114743c5a95f8cf145
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2024-06-17 20:42:27 +0300

    Added Tableview to view users and sort functionality for sorting data.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,36 @@
 import Dropzone from '@/components/Dropzone';
+import TableWrapper from '@/components/table/TableWrapper';
+import { db } from '@/firebase';
+import { FileType } from '@/typings';
 import { auth } from '@clerk/nextjs';
-import React from 'react';
+import { collection, getDocs } from 'firebase/firestore';
 
-function Dashboard() {
+async function Dashboard() {
   const { userId } = auth();
+
+  const docResults = await getDocs(collection(db, 'users', userId!, 'files'));
+  const skeletonFiles: FileType[] = docResults.docs.map((doc) => ({
+    id: doc.id,
+    filename: doc.data().filename || doc.id,
+    timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    fullName: doc.data().fullName,
+    downloadURL: doc.data().downloadURL,
+    type: doc.data().type,
+    size: doc.data().size,
+  }));
+
+  console.log(skeletonFiles);
+
   return (
     <div>
-      {/* <h1>Dashboard</h1> */}
       <Dropzone />
+      <section className='container space-y-5'>
+        <h2 className='font-bold'>All Files</h2>
+        <div>
+          {/* Table Wrapper */}
+          <TableWrapper skeletonFiles={skeletonFiles} />
+        </div>
+      </section>
     </div>
   );
 }
---
+++ components/table/TableWrapper.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { FileType } from '@/typings';
+import { Button } from '../ui/button';
+import { DataTable } from './Table';
+import { columns } from './columns';
+import { useUser } from '@clerk/nextjs';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { collection, orderBy, query } from 'firebase/firestore';
+import { db } from '@/firebase';
+
+function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
+  const { user } = useUser();
+  const [initialFiles, setInitialFiles] = useState<FileType[]>([]);
+  const [sort, setSort] = useState<'asc' | 'desc'>('desc');
+
+  const [docs, loading, error] = useCollection(
+    user &&
+      query(
+        collection(db, 'users', user.id, 'files'),
+        orderBy('timestamp', sort)
+      )
+  );
+
+  useEffect(() => {
+    if (!docs) return;
+
+    const files = docs.docs.map((doc) => ({
+      id: doc.id,
+      filename: doc.data().filename || doc.id,
+      timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+      fullName: doc.data().fullName,
+      downloadURL: doc.data().downloadURL,
+      type: doc.data().type,
+      size: doc.data().size,
+    }));
+
+    setInitialFiles(files);
+  }, [docs]);
+
+  return (
+    <div className=''>
+      <Button onClick={() => setSort(sort === 'desc' ? 'asc' : 'desc')}>
+        Sort by {sort === 'desc' ? 'Newest' : 'Oldest'}
+      </Button>
+      <DataTable columns={columns} data={initialFiles} />
+    </div>
+  );
+}
+
+export default TableWrapper;
